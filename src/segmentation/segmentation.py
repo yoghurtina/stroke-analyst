7f250bcc670cbff23f978ac01117c13a442087d6
Yogurtina <ioannagemou@gmail.com>
2023-02-20 12:30:36 +0200

    background segmentation scripts

---
+++ src/segmentation/segmentation.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load the image
+img = cv2.imread("test2.jpeg")
+# img = cv2.imread("13.png")
+# img = cv2.imread("5.png")
+
+# Convert to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Apply Canny edge detection
+edges = cv2.Canny(gray, 20, 200)
+
+# Apply dilation to thicken the edges
+kernel = np.ones((5,5),np.uint8)
+dilation = cv2.dilate(edges, kernel, iterations=1)
+
+# Find the contours of the object
+contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# Find the largest contour
+largest_contour = max(contours, key=cv2.contourArea)
+
+# Create a mask of the object
+mask = np.zeros_like(gray)
+cv2.drawContours(mask, [largest_contour], 0, (255, 255, 255), -1)
+
+# Apply the mask to the original image
+result = cv2.bitwise_and(img, img, mask=mask)
+
+# Show the result
+plt.imshow(result[:,:,::-1])
+plt.show()

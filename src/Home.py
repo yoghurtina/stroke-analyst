ecae5b5cc0ad0f4823cd453ce07eb817b62f3f3c
Yogurtina <ioannagemou@gmail.com>
2023-02-23 00:11:26 +0200

    experimentation with streamlit

---
+++ src/Home.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+import collections.abc
+collections.Iterable = collections.abc.Iterable
+collections.Mapping = collections.abc.Mapping
+collections.MutableSet = collections.abc.MutableSet
+collections.MutableMapping = collections.abc.MutableMapping
+
+
+"""
+# Stroke Analyst
+__Under Construction__
+"""

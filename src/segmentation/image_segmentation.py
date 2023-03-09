04d3d44612823b13f92b7b1e34a4bbf004dfb8cf
Yogurtina <ioannagemou@gmail.com>
2023-03-09 15:51:08 +0200

    plotted the internal stages of the seg alg

--- src/segmentation/image_segmentation.py
+++ src/segmentation/image_segmentation.py
@@ -2,47 +2,65 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from PIL import Image
 
 # Load the image
 path1 = "data/good sections"
 path2 = "data/bad sections"
 
 def segmentation(image_file):
-    img = cv2.imread(image_file)
+    img = Image.open(image_file).convert("RGB")
+    img = np.array(img)
    
     # Convert to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # plt.imshow(gray)
+    # plt.show()
 
     # Apply Canny edge detection
     edges = cv2.Canny(gray, 20, 200)
+    # plt.imshow(edges)
+    # plt.show()
+
 
     # Apply dilation to thicken the edges
     kernel = np.ones((5,5),np.uint8)
     dilation = cv2.dilate(edges, kernel, iterations=1)
+    # plt.imshow(dilation)
+    # plt.show()
+
 
     # Find the contours of the object
     contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     # Find the largest contour
     largest_contour = max(contours, key=cv2.contourArea)
+    # plt.imshow(hierarchy)
+    # plt.show()
 
     # Create a mask of the object
     mask = np.zeros_like(gray)
-    cv2.drawContours(mask, [largest_contour], 0, (255, 255, 255), -1)
 
+    cv2.drawContours(mask, [largest_contour], 0, (255, 255, 255), -1)
+    # plt.imshow(mask)
+    # plt.show()
+    
     # Apply erosion to remove noise in outer edges
     kernel = np.ones((7,7),np.uint8)
-    # mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    # mask = cv2.morphologyEx(mask, cv2.MORPH_ERODE, kernel)
     mask = cv2.erode(mask, kernel, iterations=1)
+    # plt.imshow(mask)
+    # plt.show()
 
     # Apply the mask to the original image
     result = cv2.bitwise_and(img, img, mask=mask)
-
-    # Show the result
-    # plt.imshow(result[:,:,::-1])
+    # plt.imshow(result)
     # plt.show()
 
+    # result = cv2.cvtColor(result,cv2.COLOR_RGB2BGR) 
+    # plt.show()
     return result, mask
+
 
 # Set the path to the folder where you want to save the processed images
 output_folder_path1 = "data/results good"

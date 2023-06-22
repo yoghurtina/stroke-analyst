36b799afab8cca8a708734960f19bf8ca09e0690
Ioanna Gemou <ioannagemou@gmail.com>
2023-06-22 18:52:35 +0300

    fix some issues in naming and volumetry

--- src/module/compute_voldata.py
+++ src/module/compute_voldata.py
@@ -26,8 +26,11 @@ def compute_volumetric_data(whole_mask, lesion_mask, left_hem_mask, right_hem_ma
     y = [i for i, val in enumerate(right_hem_array.flatten()) if val != 0]
     rh_area = len(y) * pix_area
 
-    variable = ['whole_mask_area','lesion_area', 'left_hem_area', 'right_hem_area']
-    value = [ mask_area, lesion_area, lh_area, rh_area]
+    # variable = ['whole_mask_area','lesion_area', 'left_hem_area', 'right_hem_area']
+    # value = [ mask_area, lesion_area, lh_area, rh_area]
+
+    variable = ['lesion_area', 'ischemic_hem_area', 'healthy_hem_area']
+    value = [ lesion_area, lh_area, rh_area]
 
     data = {'Variable': variable, 'Value': value}
     df = pd.DataFrame(data)
@@ -36,47 +39,54 @@ def compute_volumetric_data(whole_mask, lesion_mask, left_hem_mask, right_hem_ma
     df.to_csv(save_dir + '/results.csv', index=False)
 
 
-def compute_volumetric_data(whole_mask, lesion_mask, left_hem_mask, right_hem_mask, save_dir):
-    pix_area = 0.021 * 0.021
-    whole_mask = Image.open(whole_mask)
-    lesion_mask = Image.open(lesion_mask)
-    left_hem_mask = Image.open(left_hem_mask)
-    right_hem_mask = Image.open(right_hem_mask)
+# compute_volumetric_data("/home/ioanna/Documents/Thesis/src/temp/segmentation/mask_segmented_image.jpg","/home/ioanna/Documents/Thesis/src/temp/detection/mask3_hem1.jpg",\
+#                          "/home/ioanna/Documents/Thesis/src/temp/detection/mask1_hem1.jpg", "/home/ioanna/Documents/Thesis/src/temp/detection/mask1_hem2.jpg", "/home/ioanna/Documents/Thesis/src/temp")
 
-    mask_array = np.array(whole_mask)
-    lesion_array = np.array(lesion_mask)
-    left_hem_array = np.array(left_hem_mask)
-    right_hem_array = np.array(right_hem_mask)
+# def compute_volumetric_data(whole_mask, lesion_mask, left_hem_mask, right_hem_mask, save_dir):
+#     pix_area = 0.021 * 0.021
+#     whole_mask = Image.open(whole_mask)
+#     lesion_mask = Image.open(lesion_mask)
+#     left_hem_mask = Image.open(left_hem_mask)
+#     right_hem_mask = Image.open(right_hem_mask)
 
-    y = [i for i, val in enumerate(mask_array.flatten()) if val != 0]
-    mask_area = len(y) * pix_area
+#     mask_array = np.array(whole_mask)
+#     lesion_array = np.array(lesion_mask)
+#     left_hem_array = np.array(left_hem_mask)
+#     right_hem_array = np.array(right_hem_mask)
 
-    y = [i for i, val in enumerate(lesion_array.flatten()) if val != 0]
-    lesion_area = len(y) * pix_area
+#     y = [i for i, val in enumerate(mask_array.flatten()) if val != 0]
+#     mask_area = len(y) * pix_area
 
-    y = [i for i, val in enumerate(left_hem_array.flatten()) if val != 0]
-    lh_area = len(y) * pix_area
+#     y = [i for i, val in enumerate(lesion_array.flatten()) if val != 0]
+#     lesion_area = len(y) * pix_area
 
-    y = [i for i, val in enumerate(right_hem_array.flatten()) if val != 0]
-    rh_area = len(y) * pix_area
+#     y = [i for i, val in enumerate(left_hem_array.flatten()) if val != 0]
+#     lh_area = len(y) * pix_area
 
-    # Calculate volumes
-    slice_thickness = 0.021  # Adjust according to the actual thickness of the slice
-    lh_volume = lh_area * slice_thickness
-    rh_volume = rh_area * slice_thickness
-    infarct_volume = lesion_area * slice_thickness
+#     y = [i for i, val in enumerate(right_hem_array.flatten()) if val != 0]
+#     rh_area = len(y) * pix_area
 
-    # Calculate percentage volumes
-    total_volume = lh_volume + rh_volume + infarct_volume
-    v_inf = ((rh_volume + infarct_volume - lh_volume) / rh_volume) * 100
-    v_he = ((lh_volume - rh_volume) / rh_volume) * 100
+#     # Calculate volumes
+#     slice_thickness = 0.021  # Adjust according to the actual thickness of the slice
+#     lh_volume = lh_area * slice_thickness
+#     rh_volume = rh_area * slice_thickness
+#     infarct_volume = lesion_area * slice_thickness
 
-    # Create dataframe
-    variable = ['whole_mask_area', 'lesion_area', 'left_hem_area', 'right_hem_area', 'left_hem_volume', 'right_hem_volume', 'infarct_volume', 'v_inf', 'v_he']
-    value = [mask_area, lesion_area, lh_area, rh_area, lh_volume, rh_volume, infarct_volume, v_inf, v_he]
-    data = {'Variable': variable, 'Value': value}
-    df = pd.DataFrame(data)
+#     # Calculate percentages
+#     v_inf = ((rh_volume + infarct_volume - lh_volume) / rh_volume) * 100
+#     v_he = ((lh_volume - rh_volume) / rh_volume) * 100
 
-    # Print and save the results
-    print(df)
-    df.to_csv(save_dir + '/results.csv', index=False)
+#     # Create dataframe
+#     variable = ['whole_mask_area', 'lesion_area', 'left_hem_area', 'right_hem_area']
+#     value = [mask_area, lesion_area, lh_area, rh_area]
+#     data = {'Variable': variable, 'Value': value}
+#     df = pd.DataFrame(data)
+
+#     # Add volumes and percentages to the dataframe
+#     df.loc[0, 'infarct_volume'] = infarct_volume
+#     df.loc[0, 'v_inf'] = v_inf
+#     df.loc[0, 'v_he'] = v_he
+
+#     # Print and save the results
+#     print(df)
+#     df.to_csv(save_dir + '/results.csv', index=False)
--- src/module/region_naming.py
+++ src/module/region_naming.py
@@ -34,12 +34,12 @@ def extract_allen_slice(index):
     return slice
 
 import matplotlib.pyplot as plt
-print(extract_allen_slice(308))
-array = extract_allen_slice(308)
-print(type(array))
+# print(extract_allen_slice(308))
+# array = extract_allen_slice(308)
+# print(type(array))
 
-plt.imshow(array, cmap='gray')
-plt.show()
+# plt.imshow(array, cmap='gray')
+# plt.show()
 
 def extract_allen_mask(index):
     size = [528, 320, 456]
@@ -60,13 +60,13 @@ def extract_allen_mask(index):
     
     return slice
 
-import matplotlib.pyplot as plt
-print(extract_allen_mask(308))
-array = extract_allen_mask(308)
-print(type(array))
+# import matplotlib.pyplot as plt
+# print(extract_allen_mask(308))
+# array = extract_allen_mask(308)
+# print(type(array))
 
-plt.imshow(array, cmap='coolwarm')
-plt.show()
+# plt.imshow(array, cmap='coolwarm')
+# plt.show()
 
 def find_region_name(json_data, label):
     # Recursive function to search in a JSON structure for the 'id' value and return the corresponding 'name'
@@ -122,8 +122,8 @@ def region_naming(json_data, bregma_distance, lesion_AS, save_dir):
     # plt.show()
     
     target_height, target_width = ori_allen_labels.shape
-    print(target_height, target_width)
-    print(target_height, target_width)
+    # print(target_height, target_width)
+    # print(target_height, target_width)
     lesion_AS = cv2.resize(lesion_AS, (target_width, target_height))
 
     # print(lesion_AS.shape)
@@ -160,15 +160,61 @@ def region_naming(json_data, bregma_distance, lesion_AS, save_dir):
 
     return None
 
+def compare_region_names(ground_truth_regions, predicted_regions):
+    common_regions = set(ground_truth_regions) & set(predicted_regions)
+    missing_regions = set(ground_truth_regions) - set(predicted_regions)
+    extra_regions = set(predicted_regions) - set(ground_truth_regions)
+    
+    print("Common regions:")
+    print(common_regions)
+    
+    print("Missing regions:")
+    print(missing_regions)
+    
+    print("Extra regions:")
+    print(extra_regions)
+
+
+def calculate_region_iou(ground_truth_regions, predicted_regions):
+    intersection = len(set(ground_truth_regions) & set(predicted_regions))
+    union = len(set(ground_truth_regions) | set(predicted_regions))
+    
+    iou = intersection / union
+    return iou
 
 # # Load JSON data from file and convert to dictionary
-# with open('/raw_data/acronyms.json') as file:
+# with open('/home/ioanna/Documents/Thesis/raw_data/acronyms.json') as file:
 #     json_data = json.load(file)
 #     # json_data = {item['id']: item for item in json_data}
 # print(json_data)
 
-# lesion_AS = Image.open('/src/module/mask3_hem1.jpg')
-# allen_masks = "/raw_data/allen_masks/-2.7.csv"
-# 
-# result = region_naming(json_data, allen_masks,-2.68e-3, lesion_AS, "/home/ioanna/Documents/Thesis/src/module")
-# print(result)
+# predicted_lesion = Image.open('/home/ioanna/Documents/Thesis/results/validation/volumetry/stroke/computed_masks/17stroke.jpg')
+# gt_lesion = Image.open('/home/ioanna/Documents/Thesis/results/validation/volumetry/stroke/ground_truth_masks/mask_se_17.jpg')
+
+# ground_truth_regions = region_naming(json_data,2.22e-3, predicted_lesion, "/home/ioanna/Documents/Thesis/src/module")
+# print(ground_truth_regions)
+# predicted_regions = region_naming(json_data,2.22e-3, gt_lesion, "/home/ioanna/Documents/Thesis/src/module")
+# print(predicted_regions)
+# iou = calculate_region_iou(ground_truth_regions, predicted_regions)
+# print("IoU:", iou)
+
+# # 0.43 1.82
+# # 0.32 0.30
+# # 0.22 0.48
+# # -0.18 0.39
+# # -1.58 0.65
+# # 1.92 0.38
+# # -0.18 0.47
+# # -1.38 0.62
+# # -0.38 0.46
+# # -0.48 0.43
+# # 2.22 0.69
+
+
+
+# # lesion_AS = Image.open('/src/module/mask3_hem1.jpg')
+# # allen_masks = "/raw_data/allen_masks/-2.7.csv"
+# # 
+# # result = region_naming(json_data, allen_masks,-2.68e-3, lesion_AS, "/home/ioanna/Documents/Thesis/src/module")
+# # print(result)
+

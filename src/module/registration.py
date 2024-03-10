7a61c49dcd2cf11403c8a78d70b55801c7badffd
Ioanna Gemou <ioannagemou@gmail.com>
2024-03-10 23:02:02 +0100

    Fix SimpleElasix bugs, moved to SimpleITK

--- src/module/registration.py
+++ src/module/registration.py
@@ -11,61 +11,147 @@ def resampler(moving_image, fixed_image):
     # sitk.WriteImage(resampled_image, "/home/ioanna/Documents/Thesis/src/registration/reg_results/elastix/resampled_image.nii")
     return resampled_image
 
-# moving_image = resampler(moving_image, fixed_image)
+# # moving_image = resampler(moving_image, fixed_image)
+
+def rigid(fixed_image_path, moving_image_path, output_path="src/temp/registration/rigid_result.nii"):
+    fixed_image = sitk.ReadImage(fixed_image_path, sitk.sitkFloat32)
+    moving_image = sitk.ReadImage(moving_image_path, sitk.sitkFloat32)
+
+    initial_transform = sitk.CenteredTransformInitializer(fixed_image, moving_image, sitk.Euler2DTransform(), sitk.CenteredTransformInitializerFilter.GEOMETRY)
+    registration_method = sitk.ImageRegistrationMethod()
+    registration_method.SetMetricAsMattesMutualInformation(numberOfHistogramBins=50)
+    registration_method.SetMetricSamplingStrategy(registration_method.RANDOM)
+    registration_method.SetMetricSamplingPercentage(0.01)
+    registration_method.SetInterpolator(sitk.sitkLinear)
+    registration_method.SetOptimizerAsGradientDescent(learningRate=1.0, numberOfIterations=100)
+    registration_method.SetOptimizerScalesFromPhysicalShift()
+    registration_method.SetInitialTransform(initial_transform, inPlace=False)
+
+    final_transform = registration_method.Execute(fixed_image, moving_image)
+    result_image = sitk.Resample(moving_image, fixed_image, final_transform, sitk.sitkLinear, 0.0, moving_image.GetPixelID())
+    sitk.WriteImage(result_image, output_path)
+    return result_image
+
+def affine(fixed_image_path, moving_image_path, output_path="src/temp/registration/affine_result.nii"):
+    fixed_image = sitk.ReadImage(fixed_image_path, sitk.sitkFloat32)
+    moving_image = sitk.ReadImage(moving_image_path, sitk.sitkFloat32)
+
+    initial_transform = sitk.CenteredTransformInitializer(fixed_image, moving_image, sitk.AffineTransform(2), sitk.CenteredTransformInitializerFilter.GEOMETRY)
+    registration_method = sitk.ImageRegistrationMethod()
+    registration_method.SetMetricAsMattesMutualInformation(numberOfHistogramBins=50)
+    registration_method.SetMetricSamplingStrategy(registration_method.RANDOM)
+    registration_method.SetMetricSamplingPercentage(0.01)
+    registration_method.SetInterpolator(sitk.sitkLinear)
+    registration_method.SetOptimizerAsGradientDescent(learningRate=1.0, numberOfIterations=100)
+    registration_method.SetOptimizerScalesFromPhysicalShift()
+    registration_method.SetInitialTransform(initial_transform, inPlace=False)
+
+    final_transform = registration_method.Execute(fixed_image, moving_image)
+    result_image = sitk.Resample(moving_image, fixed_image, final_transform, sitk.sitkLinear, 0.0, moving_image.GetPixelID())
+    sitk.WriteImage(result_image, output_path)
+    return result_image
+
+def non_rigid(fixed_image_path, moving_image_path, output_path="src/temp/registration/non_rigid_result.nii"):
+    fixed_image = sitk.ReadImage(fixed_image_path, sitk.sitkFloat32)
+    moving_image = sitk.ReadImage(moving_image_path, sitk.sitkFloat32)
+
+    # Initialize B-Spline transform
+    transform = sitk.BSplineTransformInitializer(fixed_image, [8, 8, 8], order=3)
+
+    # Set up the registration method
+    registration_method = sitk.ImageRegistrationMethod()
+
+    # Use Mattes Mutual Information as the metric
+    registration_method.SetMetricAsMattesMutualInformation(numberOfHistogramBins=50)
+    registration_method.SetMetricSamplingStrategy(registration_method.RANDOM)
+    registration_method.SetMetricSamplingPercentage(0.01)
+
+    # Use linear interpolation
+    registration_method.SetInterpolator(sitk.sitkLinear)
+
+    # Setup for the optimizer
+    registration_method.SetOptimizerAsGradientDescent(learningRate=1.0, numberOfIterations=200)
+    registration_method.SetOptimizerScalesFromPhysicalShift()
+
+    # Multi-resolution framework setup
+    registration_method.SetShrinkFactorsPerLevel(shrinkFactors=[4, 2, 1])
+    registration_method.SetSmoothingSigmasPerLevel(smoothingSigmas=[2, 1, 0])
+    registration_method.SmoothingSigmasAreSpecifiedInPhysicalUnitsOn()
+
+    # Set the initial transform
+    registration_method.SetInitialTransform(transform, inPlace=False)
+
+    # Execute the registration
+    final_transform = registration_method.Execute(fixed_image, moving_image)
+
+    # Apply the final transform to the moving image
+    result_image = sitk.Resample(moving_image, fixed_image, final_transform, sitk.sitkLinear, 0.0, moving_image.GetPixelID())
+
+    # Save the result
+    sitk.WriteImage(result_image, output_path)
+
+    return result_image
 
 
-def rigid(fixed_image_path, moving_image_path):
-    elastixImageFilter = sitk.ElastixImageFilter()
-    elastixImageFilter.LogToFileOn()
+def advanced_non_rigid(fixed_image_path, moving_image_path, output_path="src/temp/registration/non_rigid_advanced.nii"):
+    # Read the fixed and moving images
+    fixed_image = sitk.ReadImage(fixed_image_path, sitk.sitkFloat32)
+    moving_image = sitk.ReadImage(moving_image_path, sitk.sitkFloat32)
 
-    elastixImageFilter.SetFixedImage(sitk.ReadImage(fixed_image_path))
-    elastixImageFilter.SetMovingImage(sitk.ReadImage(moving_image_path))
-    elastixImageFilter.SetParameterMap(sitk.GetDefaultParameterMap("rigid"))
-    elastixImageFilter.Execute()
-    sitk.WriteImage(elastixImageFilter.GetResultImage(), "src/temp/registration/rigid.nii")
+    # Initial transformation (affine) for alignment
+    initial_transform = sitk.CenteredTransformInitializer(fixed_image, 
+                                                          moving_image, 
+                                                          sitk.AffineTransform(fixed_image.GetDimension()),
+                                                          sitk.CenteredTransformInitializerFilter.GEOMETRY)
 
-    return elastixImageFilter.GetResultImage()
+    # Multi-resolution framework for image registration
+    registration_method = sitk.ImageRegistrationMethod()
 
-def affine(fixed_image_path, moving_image_path):
-    elastixImageFilter = sitk.ElastixImageFilter()
-    elastixImageFilter.LogToFileOn()
+    # Similarity metric settings
+    registration_method.SetMetricAsMattesMutualInformation(numberOfHistogramBins=50)
+    registration_method.SetMetricSamplingStrategy(registration_method.RANDOM)
+    registration_method.SetMetricSamplingPercentage(0.01, seed=1)
 
-    elastixImageFilter.SetFixedImage(sitk.ReadImage(fixed_image_path))
-    elastixImageFilter.SetMovingImage(sitk.ReadImage(moving_image_path))
-    elastixImageFilter.SetParameterMap(sitk.GetDefaultParameterMap("affine"))
-    elastixImageFilter.Execute()
-    sitk.WriteImage(elastixImageFilter.GetResultImage(),  "src/temp/registration/affine.nii")
+    # Interpolator
+    registration_method.SetInterpolator(sitk.sitkLinear)
 
-    return elastixImageFilter.GetResultImage()
+    # Optimizer settings
+    registration_method.SetOptimizerAsGradientDescent(learningRate=1.0, 
+                                                      numberOfIterations=100, 
+                                                      convergenceMinimumValue=1e-6, 
+                                                      convergenceWindowSize=10)
+    registration_method.SetOptimizerScalesFromPhysicalShift()
 
-def non_rigid(fixed_image_path, moving_image_path):
-    elastixImageFilter = sitk.ElastixImageFilter()
-    elastixImageFilter.LogToFileOn()
+    # Setup for multi-resolution strategy
+    registration_method.SetShrinkFactorsPerLevel(shrinkFactors = [4, 2, 1])
+    registration_method.SetSmoothingSigmasPerLevel(smoothingSigmas=[2, 1, 0])
+    registration_method.SmoothingSigmasAreSpecifiedInPhysicalUnitsOn()
 
-    elastixImageFilter.SetFixedImage(sitk.ReadImage(fixed_image_path))
-    elastixImageFilter.SetMovingImage(sitk.ReadImage(moving_image_path))
+    # Use the initial affine transform and then refine with B-Splines
+    registration_method.SetInitialTransform(initial_transform, inPlace=False)
 
-    # Configure the parameter map for affine registration
-    parameterMapVector = sitk.VectorOfParameterMap()
-    parameterMapVector.append(sitk.GetDefaultParameterMap("affine"))
+    # Final B-Spline transformation (non-rigid)
+    transform_to_displacement_field_filter = sitk.TransformToDisplacementFieldFilter()
+    transform_to_displacement_field_filter.SetReferenceImage(fixed_image)
+    transform_to_displacement_field_filter.SetInterpolator(sitk.sitkLinear)
 
-    # Configure the parameter map for B-spline registration
-    bspline_parameter_map = sitk.GetDefaultParameterMap("bspline")
+    bspline_transform = sitk.BSplineTransformInitializer(fixed_image, 
+                                                         transformDomainMeshSize=[8,8,8], 
+                                                         order=3)
+    registration_method.SetInitialTransform(bspline_transform, inPlace=False)
 
-    # Modify B-spline parameters for smoother deformation and image pyramid
-    bspline_parameter_map['ImagePyramidSchedule'] = ['32, 16, 8, 6, 4, 2, 1']  # Image pyramid schedule with decreasing scale factors
-    bspline_parameter_map['BSplineSmoothingSigma'] = ['10']  # Increase the smoothing sigma value for smoother deformation
-    bspline_parameter_map['FinalGridSpacingInPhysicalUnits'] = ['32, 16, 10, 8, 6, 4, 2, 1']
-        # bspline_parameter_map['FinalGridSpacingInPhysicalUnits'] = ['32, 16, 10, 8, 6, 4, 2']
+    # Execute the registration
+    final_transform = registration_method.Execute(fixed_image, moving_image)
 
-    parameterMapVector.append(bspline_parameter_map)
+    # Apply the final transform to resample the moving image
+    resampler = sitk.ResampleImageFilter()
+    resampler.SetReferenceImage(fixed_image)
+    resampler.SetInterpolator(sitk.sitkLinear)
+    resampler.SetTransform(final_transform)
+    resampler.SetDefaultPixelValue(100)
+    resampled_image = resampler.Execute(moving_image)
 
-    # Set the parameter map and execute the registration
-    elastixImageFilter.SetParameterMap(parameterMapVector)
-    elastixImageFilter.Execute()
+    # Save the result
+    sitk.WriteImage(resampled_image, output_path)
 
-    # Save the resulting image
-    sitk.WriteImage(elastixImageFilter.GetResultImage(), "src/temp/registration/non_rigid.nii")
-
-    return elastixImageFilter.GetResultImage()
-
+    return output_path
